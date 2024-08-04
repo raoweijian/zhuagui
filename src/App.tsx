f39823c4286916b83a1d3f91abf45a8fb1416e96
Weijian Rao <weijianrao@gmail.com>
2024-08-04 20:20:53 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,10 @@ import TextField from '@mui/material/TextField';
 
 import bxMap from "./assets/bx.png";
 import jyMap from "./assets/jy.png";
+import alMap from "./assets/al.png";
+import jingwaiMap from "./assets/jingwai.png";
+import jnywMap from "./assets/jnyw.png";
+import nvercunMap from "./assets/nvercun.png";
 
 interface Point {
   x: number;
@@ -22,6 +26,7 @@ interface Map {
   src: any;
   xMax: number;
   yMax: number;
+  scale: number;
 }
 
 const MAPS: Map[] = [
@@ -30,13 +35,43 @@ const MAPS: Map[] = [
     src: bxMap,
     xMax: 159,
     yMax: 119,
+    scale: 1,
   },
   {
     name: "建邺城",
     src: jyMap,
     xMax: 287,
     yMax: 142,
-  }
+    scale: 1,
+  },
+  {
+    name: "傲来国",
+    src: alMap,
+    xMax: 222,
+    yMax: 150,
+    scale: 1,
+  },
+  {
+    name: "大唐境外",
+    src: jingwaiMap,
+    xMax: 638,
+    yMax: 118,
+    scale: 1,
+  },
+  {
+    name: "江南野外",
+    src: jnywMap,
+    xMax: 159,
+    yMax: 119,
+    scale: 1,
+  },
+  {
+    name: "女儿村",
+    src: nvercunMap,
+    xMax: 127,
+    yMax: 143,
+    scale: 1,
+  },
 ];
 
 function App() {
@@ -46,15 +81,29 @@ function App() {
   const mapOptions = MAPS.map(map => map.name);
 
   return (
-    <Box sx={{margin: 10}}>
-      <Grid container spacing={10}>
-        <Grid xs={5}>
-          <Box justifyContent="space-between" display="flex" flexDirection="row">
-            <TextField type="number" label="横坐标" variant="outlined" value={x} onChange={change => setX(parseInt(change.target.value))}/>
-            <TextField type="number" label="纵坐标" variant="outlined" value={y} onChange={change => setY(parseInt(change.target.value))}/>
+    <Box sx={{marginLeft: 5}}>
+      <Box sx={{ margin: 10, display: 'flex', flexDirection: 'row', height: 500 }}>
+        <Box sx={{ flex: '0 0 300px', display: 'flex', flexDirection: 'column' }}>
+          <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}
+          >
+            <TextField
+              type="number"
+              label="横坐标"
+              variant="outlined"
+              value={x}
+              onChange={change => setX(parseInt(change.target.value))}
+            />
+            <TextField
+              type="number"
+              label="纵坐标"
+              variant="outlined"
+              value={y}
+              onChange={change => setY(parseInt(change.target.value))}
+              sx={{ marginLeft: 2 }}
+            />
           </Box>
 
-          <Box justifyContent="start" display="flex" flexDirection="row" sx={{marginTop: 5}}>
+          <Box sx={{ marginTop: 5 }}>
             <FormControl>
               <FormLabel id="demo-radio-buttons-group-label">地图</FormLabel>
               <RadioGroup
@@ -73,20 +122,18 @@ function App() {
               </RadioGroup>
             </FormControl>
           </Box>
-        </Grid>
+        </Box>
 
-        <Grid xs={7}>
-          <Box justifyContent="center" display="flex" flexDirection="row">
-            {selectedMap && (
-              <ImageWithOverlay
-                x={x}
-                y={y}
-                map={selectedMap}
-              />
-            )}
-          </Box>
-        </Grid>
-      </Grid>
+        <Box sx={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
+          {selectedMap && (
+            <ImageWithOverlay
+              x={x}
+              y={y}
+              map={selectedMap}
+            />
+          )}
+        </Box>
+      </Box>
     </Box>
   )
 }
@@ -101,13 +148,12 @@ const ImageWithOverlay = ({
   map: Map,
 }) => {
   const canvasRef = useRef(null);
-  const scale = 0.5;
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d');
     const image = new Image();
-    const {xMax, yMax} = map;
+    const {xMax, yMax, scale} = map;
 
     image.src = map.src;
     image.onload = () => {
@@ -116,15 +162,15 @@ const ImageWithOverlay = ({
 
       canvas.width = scaledWidth;
       canvas.height = scaledHeight;
+      console.log('scaledWidth', scaledWidth, 'scaledHeight', scaledHeight);
 
       context.drawImage(image, 0, 0, scaledWidth, scaledHeight);
 
       context.strokeStyle = 'red'; // 不透明红色
-      context.lineWidth = 4; // 设置边框宽度
+      context.lineWidth = 5; // 设置边框宽度
 
       const topLeft = getTopLeft({x, y, yMax});
       const bottomRight = getBottomRight({x, y, xMax});
-      console.log(topLeft, bottomRight);
 
       const rectX = topLeft.x / xMax * scaledWidth;
       const rectY = (yMax - topLeft.y) / yMax * scaledHeight;
